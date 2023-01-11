93e57d27ef1ac7c08d90a1531ba9e8bc326eb109
Victor Guerra <vmgg30@gmail.com>
2023-01-10 16:32:32 -0800

    testing API integration

--- src/App.js
+++ src/App.js
@@ -1,58 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+//testing NOT WORKING
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+
+  const [data, setData] = useState('');
+  const [api, setApi] = useState('');
+  const fetchData = async () => {
+    const api = await fetch('https://jsonplaceholder.typicode.com/todos/'
+    , {
+    headers: {
+      'Authorization: Bearer': "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzIzMjcwODBjNjY4ZjQ0MDg5ODQ0NmJkYTQxNzg1ZSIsInN1YiI6IjYzYmRlOTZiZmMzMWQzMDA5MmM2NTY4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kvd3vxH0pZK8BAdfQne2lmhtdvEbTKtjIUR_qcD4ScI"
+    }
+  }
   );
+    setData(await api.json());
+    setApi (api);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+return (
+  <>
+    <h1>{api}</h1>
+    {data === '' ? (
+      <h1>Fetching todos...</h1>
+     ) : (
+       <ul>
+         {data.map(item => (
+           <li key={item.id}>{item.title}</li>
+         ))}
+      </ul>
+    )}
+  </>
+);
 }
 
 export default App;

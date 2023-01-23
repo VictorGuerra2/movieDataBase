9bba6dbf88a08df74e4632eb94c30dd34f7c6cbd
ramixca <ramixboxsv@gmail.com>
2023-01-22 22:23:25 -0800

    cleared nav component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/Header';
-import Nav from '../components/Nav';
+// import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import PageLanding from '../pages/PageLanding';
-import PageMovie from '../pages/PageMovie';
+// import PageMovie from '../pages/PageMovie';
 import PageFavourites from '../pages/PageFavourites';
 import PageAbout from '../pages/PageAbout';
 
@@ -18,13 +18,13 @@ function AppRouter() {
     <BrowserRouter>
       <div className="wrapper">
         <Header title={appTitle} />
-        <Nav />
+        {/* <Nav /> */}
         <main>
           <Routes>
             <Route path="/" exact element={<PageLanding />} />
             <Route path="/PageFavourites" element={<PageFavourites />} />
             <Route path="/PageAbout" element={<PageAbout />} />
-            <Route path="/individual" element={<PageMovie />} />
+            {/* <Route path="/individual" element={<PageMovie />} /> */}
           </Routes>
         </main>
         <Footer />

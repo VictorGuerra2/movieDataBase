5b1652be92c2bf1dc12db6f15d0885a0f0c344d6
GodsDogSled <gabekelly@hotmail.ca>
2023-02-04 12:11:08 -0800

    Formatted movie card

--- src/components/MovieCards.js
+++ src/components/MovieCards.js
@@ -18,28 +18,35 @@ function MovieCard({movieObject, isFavourite}) {
   return (
     <div>
       <div id={movieObject.id} className="movie-card">
-        <img className="movie-poster" src={`https://image.tmdb.org/t/p/w200/${movieObject.poster_path}`} alt="Movie Poster"/>
+        <div className="movie-poster">
+          <img className="movie-poster-img" src={`https://image.tmdb.org/t/p/w200/${movieObject.poster_path}`} alt="Movie Poster"/>
+          <button className="more-info" type="button">
+            <Link to="/individual" state={{from: movieObject}}>
+              More Info
+            </Link>                                     
+          </button>
+        </div>
         <div className="movie-card-text">
-        <p className="movie-title movie-text">{movieObject.title}</p>
-        <p className="movie-overview movie-text">{movieObject.overview.slice(0,99) + "..."}</p>
-        <p className="movie-release-date movie-text">Release Year:{movieObject.release_date.slice(0, 4)}</p>
-        <p className="movie-vote-average movie-text">{movieObject.vote_average} / 10</p>
-        <button type="button">
-                                    <Link to="/individual" state={{from: movieObject}}>
-                                        More Info
-                                    </Link>
-        </button>
-        
-          {isFavourite ? (
-            <FavouriteButton
-              movieObject={movieObject}
-              remove={true}
-              handleFavClick={handleFavClick}/>
-          ) : (
-            <FavouriteButton
-              movieObject={movieObject}
-              handleFavClick={handleFavClick}/>
-          )}
+          <div className="top-cluster">
+            <p className="movie-title movie-text">{movieObject.title}</p>
+            {/* <p className="movie-overview movie-text">{movieObject.overview.slice(0,99) + "..."}</p> */}
+            <p className="movie-release-date movie-text">{movieObject.release_date.slice(0, 4)}</p>
+          </div>
+          <div className="bottom-row-container">
+            <p className="movie-vote-average movie-text">{movieObject.vote_average} / 10</p>
+            
+            
+              {isFavourite ? (
+                <FavouriteButton
+                  movieObject={movieObject}
+                  remove={true}
+                  handleFavClick={handleFavClick}/>
+              ) : (
+                <FavouriteButton
+                  movieObject={movieObject}
+                  handleFavClick={handleFavClick}/>
+              )}
+            </div>
         </div>
       </div>
     </div>
